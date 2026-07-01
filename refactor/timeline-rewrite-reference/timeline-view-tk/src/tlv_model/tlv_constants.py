d53cb10c844a4f67bdbb1afa5bfa21fe71fc13a4
jamesbrown <randomvoidmail@foxmail.com>
2026-07-01 14:44:54 +0800

    update

--- refactor/timeline-rewrite-reference/timeline-view-tk/src/tlv_model/tlv_constants.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the timeline viewer data model constants.
-
-Copyright (c) Peter Triesberger
-For further information see https://codeberg.org/peter88213/timeline-view-tk
-License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
-"""
-SC_PREFIX = 'sc'
-
